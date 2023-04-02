e1350c13d45c926c5ebe16cc397b0782455e7219
wajili <95950855+wajili@users.noreply.github.com>
2023-04-03 07:32:08 +0900

    Update simmmobot.py

--- simmmobot.py
+++ simmmobot.py
@@ -4,7 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 from selenium.common.exceptions import NoSuchElementException
+import requests
+from requests.structures import CaseInsensitiveDict
+import matplotlib.pyplot as plt
 from torch.autograd import Variable
+from PIL import Image
 import os
 import utils
 import torchvision.transforms as transforms
@@ -44,6 +48,349 @@ def get_driver():
     #chrome_options.add_experimental_option("binary_location", 'home/wjl/Downloads/chrome/Linux_x64_1097615_chrome-linux/chrome-linux/chrome')
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    return driver
+    # assert "No results found." not in driver.page_source
+    # driver.close()
+
+
+def travel(driver):
+    driver.get("https://web.simple-mmo.com/travel")
+    assert "Travel" in driver.title
+
+
+def take_step(driver):
+    assert "Travel" in driver.title
+
+    while True:
+        element = driver.find_element(By.ID, "step_button")
+        element.click()
+        time.sleep(2)
+        not_machine(driver)
+        not_machine2(driver)
+        not_machine4(driver)
+        attack(driver)
+        #wave(driver)
+        collect(driver)
+
+def store_images(driver):
+    img_dir = os.listdir(UNKNOWN_IMAGE_PATH)
+    for img_name in img_dir:
+        current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+        img = cv2.imread(os.path.join(TEMP_PATH, "/", img_name))
+        cv2.imwrite(UNKNOWN_IMAGE_PATH + "/" + current_time + ".png", img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+        time.sleep(2)
+                
+                
+def not_machine(driver):
+    try:
+        driver.find_element(By.LINK_TEXT, '"pesky machine"').click()
+        #a=input('sb')
+        for counter in range(2):
+            try:
+                time.sleep(2)
+                main_window = driver.window_handles[0]
+                bot_window = driver.window_handles[1]
+                driver.switch_to.window(bot_window)
+                time.sleep(2)
+
+                assert "Player Verification" in driver.title
+                es1 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[1]/img")
+                es2 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[2]/img")
+                es3 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[3]/img")
+                es4 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[4]/img")
+                es1.screenshot(os.path.join(TEMP_PATH, "/", "img1.png"))
+                es2.screenshot(os.path.join(TEMP_PATH, "/", "img2.png"))
+                es3.screenshot(os.path.join(TEMP_PATH, "/", "img3.png"))
+                es4.screenshot(os.path.join(TEMP_PATH, "/", "img4.png"))
+                time.sleep(20)
+                element = driver.find_element(By.CSS_SELECTOR, ".text-2xl")
+                item = element.text.lower()
+                prediction = verifier.simmmover(TEMP_PATH, item)
+
+                if prediction == 'img1':
+                    es1.click()
+                elif prediction == 'img2':
+                    es2.click()
+                elif prediction == 'img3':
+                    es3.click()
+                elif prediction == 'img4':
+                    es4.click()
+            
+                time.sleep(3)
+                try:
+                    driver.find_element(By.LINK_TEXT, "Retry").click()
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("failed####" + " " + item + " " + current_time)
+                    store_images(driver)
+                    if counter >= 1:
+                        b = input('sb')
+                    continue
+            
+                except NoSuchElementException:
+                    driver.find_element(By.ID, "swal2-title")
+                    driver.close()
+                    driver.switch_to.window(main_window)
+                    #store_images(driver)
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("success!!!" + " " + current_time)
+                    return
+                    
+            except NoSuchElementException:
+                continue
+    except NoSuchElementException:
+        return
+
+
+def not_machine4(driver):
+
+    try:
+        driver.find_element(By.LINK_TEXT, "Press here to confirm your existence").click()
+        #a=input('sb')
+        for counter in range(2):
+            try:
+                time.sleep(2)
+                main_window = driver.window_handles[0]
+                bot_window = driver.window_handles[1]
+                driver.switch_to.window(bot_window)
+                time.sleep(2)
+
+                assert "Player Verification" in driver.title
+                es1 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[1]/img")
+                es2 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[2]/img")
+                es3 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[3]/img")
+                es4 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[4]/img")
+                es1.screenshot(os.path.join(TEMP_PATH, "/", "img1.png"))
+                es2.screenshot(os.path.join(TEMP_PATH, "/", "img2.png"))
+                es3.screenshot(os.path.join(TEMP_PATH, "/", "img3.png"))
+                es4.screenshot(os.path.join(TEMP_PATH, "/", "img4.png"))
+                time.sleep(20)
+                element = driver.find_element(By.CSS_SELECTOR, ".text-2xl")
+                item = element.text.lower()
+                prediction = verifier.simmmover(TEMP_PATH, item)
+
+                if prediction == 'img1':
+                    es1.click()
+                elif prediction == 'img2':
+                    es2.click()
+                elif prediction == 'img3':
+                    es3.click()
+                elif prediction == 'img4':
+                    es4.click()
+            
+                time.sleep(3)
+                try:
+                    driver.find_element(By.LINK_TEXT, "Retry").click()
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("failed####" + " " + item + " " + current_time)
+                    store_images(driver)
+                    if counter >= 1:
+                        b = input('sb')
+                    continue
+            
+                except NoSuchElementException:
+                    driver.find_element(By.ID, "swal2-title")
+                    driver.close()
+                    driver.switch_to.window(main_window)
+                    #store_images(driver)
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("success!!!" + " " + current_time)
+                    return
+                    
+            except NoSuchElementException:
+                continue
+    except NoSuchElementException:
+        return
+
+def not_machine2(driver):
+
+    try:
+        driver.find_element(By.LINK_TEXT, "Perform Verification").click()
+        #a=input('sb')
+        for counter in range(2):
+            try:
+                time.sleep(2)
+                main_window = driver.window_handles[0]
+                bot_window = driver.window_handles[1]
+                driver.switch_to.window(bot_window)
+                time.sleep(2)
+
+                assert "Player Verification" in driver.title
+                es1 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[1]/img")
+                es2 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[2]/img")
+                es3 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[3]/img")
+                es4 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[4]/img")
+                es1.screenshot(os.path.join(TEMP_PATH, "/", "img1.png"))
+                es2.screenshot(os.path.join(TEMP_PATH, "/", "img2.png"))
+                es3.screenshot(os.path.join(TEMP_PATH, "/", "img3.png"))
+                es4.screenshot(os.path.join(TEMP_PATH, "/", "img4.png"))
+                time.sleep(20)
+                element = driver.find_element(By.CSS_SELECTOR, ".text-2xl")
+                item = element.text.lower()
+                prediction = verifier.simmmover(TEMP_PATH, item)
+
+                if prediction == 'img1':
+                    es1.click()
+                elif prediction == 'img2':
+                    es2.click()
+                elif prediction == 'img3':
+                    es3.click()
+                elif prediction == 'img4':
+                    es4.click()
+            
+                time.sleep(3)
+                try:
+                    driver.find_element(By.LINK_TEXT, "Retry").click()
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("failed####" + " " + item + " " + current_time)
+                    store_images(driver)
+                    if counter >= 1:
+                        b = input('sb')
+                    continue
+            
+                except NoSuchElementException:
+                    driver.find_element(By.ID, "swal2-title")
+                    driver.close()
+                    driver.switch_to.window(main_window)
+                    #store_images(driver)
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("success!!!" + " " + current_time)
+                    return
+                    
+            except NoSuchElementException:
+                continue
+    except NoSuchElementException:
+        return
+
+def not_machine3(driver):
+    try:
+        deter = 0
+        driver.find_element(By.LINK_TEXT, "Press here to verify").click()
+        #a=input('sb')
+        for counter in range(2):
+            try:
+                time.sleep(2)
+                main_window = driver.window_handles[0]
+                bot_window = driver.window_handles[1]
+                driver.switch_to.window(bot_window)
+                time.sleep(2)
+
+                assert "Player Verification" in driver.title
+                es1 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[1]/img")
+                es2 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[2]/img")
+                es3 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[3]/img")
+                es4 = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div/div[3]/div[4]/img")
+                es1.screenshot(os.path.join(TEMP_PATH, "/", "img1.png"))
+                es2.screenshot(os.path.join(TEMP_PATH, "/", "img2.png"))
+                es3.screenshot(os.path.join(TEMP_PATH, "/", "img3.png"))
+                es4.screenshot(os.path.join(TEMP_PATH, "/", "img4.png"))
+                time.sleep(20)
+                element = driver.find_element(By.CSS_SELECTOR, ".text-2xl")
+                item = element.text.lower()
+                prediction = verifier.simmmover(TEMP_PATH, item)
+
+                if prediction == 'img1':
+                    es1.click()
+                elif prediction == 'img2':
+                    es2.click()
+                elif prediction == 'img3':
+                    es3.click()
+                elif prediction == 'img4':
+                    es4.click()
+            
+                time.sleep(3)
+                try:
+                    driver.find_element(By.LINK_TEXT, "Retry").click()
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("failed####" + " " + item + " " + current_time)
+                    store_images(driver)
+                    if counter >= 1:
+                        b = input('sb')
+                    continue
+            
+                except NoSuchElementException:
+                    driver.find_element(By.ID, "swal2-title")
+                    driver.close()
+                    driver.switch_to.window(main_window)
+                    driver.get("https://web.simple-mmo.com/travel")
+                    time.sleep(2)
+                    #store_images(driver)
+                    deter = 1
+                    current_time = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+                    print("success!!!" + " " + current_time)
+                    return deter
+                    
+            except NoSuchElementException:
+                continue
+    except NoSuchElementException:
+        return deter
+
+
+#def wave(driver):
+#    try:
+#        element = driver.find_element("xpath",
+#            "/html/body/div[1]/div[3]/main/div[2]/div/div[2]/div/div/div[6]/div[1]/div/div[2]/div/div[3]/span/button"
+#        )
+#        element.click()
+#        attack_mob(driver)
+#    except NoSuchElementException:
+#        return
+
+def attack(driver):
+    try:
+        element = driver.find_element(By.LINK_TEXT, "Attack")
+        element.click()
+        attack_mob(driver)
+    except NoSuchElementException:
+        return
+
+def collect(driver):
+    try:
+        element = driver.find_element(By.CSS_SELECTOR, "div.bg-white.overflow-hidden.shadow.rounded-lg.mx-auto.max-w-\[30rem\].dark\:bg-gray-900.dark\:border.dark\:border-gray-800 button")
+        element.click()
+        collect_mob(driver)
+    except NoSuchElementException:
+        return
+
+def collect_mob(driver):
+    #assert "Attacking" in driver.title
+    try:
+        driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div[6]/div/div[2]/div")
+        driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div[1]/nav/div/a/button").click()
+        return
+    except NoSuchElementException:
+        pass
+
+    while True:
+
+        try:
+            time.sleep(2)
+            driver.find_element(By.ID, "crafting_button").click()
+        except NoSuchElementException:
+            return
+
+def attack_mob(driver):
+    #assert "Attacking" in driver.title
+    element = driver.find_element("xpath", "/html/body/div[1]/div[3]/main/div[2]/div/div[2]/div[2]/button[1]")
+
+    while True:
+
+        try:
+            time.sleep(2)
+            deter = not_machine3(driver)
+            if deter == 1:
+                return
+            driver.find_element(By.LINK_TEXT, "End Battle").click()
+            break
+        except NoSuchElementException:
+            element.click()
+                
+
+
+
+if __name__ == "__main__":
+    main()
+
     driver = webdriver.Chrome(chrome_options=chrome_options)
     return driver
     # assert "No results found." not in driver.page_source
